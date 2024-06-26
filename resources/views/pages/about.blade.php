46b67c47de9587a7fc794a20fde2954d0bc673c9
beamaedublas <dublasbea@gmail.com>
2024-06-26 14:16:28 +0800

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('template.base')
+
+@section('content')
+    <h1 class="text-xl">About Page</h1>
+@endsection
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('template.base')
+
+@section('content')
+    <h1 class="text-xl">Contact Page</h1>
+@endsection
